4a8961965d309bcc248ccb81f4a74465f47b47bb
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-20 20:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let S = mainArr.slice();
+	let newArray = [];
+	for(let i = 0;i<S.length;i++){
+		const min1 = S[0];
+		const min2 = S[1];
+		const min3 = S[2];
+		
+		const sum = min1 + min2 + min3;
+		newArray[i] = sum;
+		
+		const toPush = S.shift();
+		S.push(toPush);
+		
+	}
+	
+	findClosestValue(newArray,target);
+	function findClosestValue(arr,target){
+	let closestValue = arr[0];
+	let minDifference = Math.abs(arr[0] - target);
+	for(let i = 1;i < arr.length;i++){
+		const difference = Math.abs(arr[i] - target);
+		if(difference<minDifference){
+			minDifference = difference;
+			closestValue = arr[i];
+		}
+	}
+	return closestValue;
+	}
 }
 
 module.exports = threeSum;
